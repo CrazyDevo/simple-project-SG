338956badcf4bd2dbab39a1b7e4e42c77377f110
AdamB25 <barsialtunit@gmail.com>
2023-01-18 17:04:08 +0300

    added git

--- src/test/java/demo_sg/StepDefs.java
+++ src/test/java/demo_sg/StepDefs.java
@@ -38,7 +38,7 @@ public class StepDefs {
 
     @Then("^I should see more results$")
     public void i_should_see_more_results() throws Throwable   {
-        System.out.println(3/0);
+       // System.out.println(3/0);
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search1"));
     }
 
